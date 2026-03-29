a2232e83f955c8eb5d6e13d032df8f77689d46cd
Angel <angelkhatriii7@gmail.com>
2026-03-29 10:26:57 +0545

    insert new user records

--- database/insertrecord.php
+++ database/insertrecord.php
@@ -1,8 +1,8 @@
 <?php
 require_once "connection.php"; //require once and require are used to include the file only once and if the file is not found it will give a fatal error and stop the execution of the script
 //include_once run the file even if there is error in the file but require once will stop execution
-$fullname="Abhinav khatri";
-$email="abhinavkhatri@example.com";
+$fullname="Anuska khatri";
+$email="anusakhatri@example.com";
 $password="abc123";
 $insertSql ="INSERT INTO users(fullname,email,password)
 VALUES(?,?,?)"; //to prevent sql injection we use prepared statement and parameterized query
