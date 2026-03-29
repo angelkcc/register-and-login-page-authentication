04eb9bf2802afdf784946667aed7259e5fac9edf
Angel <angelkhatriii7@gmail.com>
2026-03-29 10:20:53 +0545

    to display records in table

--- database/selectrecords.php
+++ database/selectrecords.php
@@ -1,4 +1,5 @@
 <?php
+/*
 require_once "connection.php";
 $selectSql="SELECT * FROM users";
 $result=$connection->query($selectSql);
@@ -9,6 +10,23 @@ if($result->num_rows==0){
 //$result->fetch_all();//for small number of data 
 //$row1=$result->fetch_assoc();//for large number of data as it fetches one row at a time
 //$row2=$result->fetch_assoc(); //again returns next row, but doesnt show the precious row so to fix this problem we use while loop
+while($row=$result->fetch_assoc()){ //this will now fetch all the rows one by one and then print them
+    echo "<pre>";
+    print_r($row);
+    echo "</pre>";
+}*/
+require_once "connection.php";
+$searchQuery="A";
+$selectSql="SELECT * FROM users where fullname LIKE ?";
+$preparedStatement=$connection->prepare($selectSql);
+$preparedStatement->bind_param('s',$searchQuery.'%');
+$preparedStatement->execute();
+$result=$preparedStatement->get_result();
+
+
+if($result->num_rows==0){
+    die("No record in the table");
+}
 while($row=$result->fetch_assoc()){ //this will now fetch all the rows one by one and then print them
     echo "<pre>";
     print_r($row);
